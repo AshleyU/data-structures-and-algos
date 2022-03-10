cfc1602fb8bd4cf268e5ada4609c5d81e7dde64b
Ashley Umetsu <ashley.umetsu@verizon.com>
2022-03-10 15:27:27 -0600

    solved countdown problem

--- algos/recursion/countdown/index.js
+++ algos/recursion/countdown/index.js
@@ -1,5 +1,12 @@
 function countdown(n){
+    let arr = ["Liftoff!"];
 
+    for(let i = 1; i <= n; i++) {
+        arr.unshift(i);
+    }
+    return arr.join(', ');
 }
+
+console.log(countdown(11));
 
 module.exports = countdown
